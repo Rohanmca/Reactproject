c557f36b6e666fa1e02e1177e6f873c2e0641ea5
Rohanmca <rohansuryawanshi001@gmail.com>
2023-09-03 20:38:11 +0530

    new react project

---
+++ reactdemo/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+ ReactDOM.render(
+  <>
+  <h1>rohan react project</h1>
+  <p>this is first project that related with all the new concept belowed in css form</p></>,
+  document.getElementById("root")
+ );
